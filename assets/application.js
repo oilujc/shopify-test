5a361de927fc6fa093096a159d1fc1ea243fbbac
Julio Martinez <jcmartinezvalero@gmail.com>
2021-01-25 14:29:59 -0400

    first commit

---
+++ assets/application.js
@@ -0,0 +1,11 @@
+// Put your application javascript here
+(function(){
+    // Your JavaScript here
+    let contactBtn = document.querySelector('.navbar #contactBtn');
+
+console.log(contactBtn);
+
+contactBtn.addEventListener('click', () => {
+    console.log('click');
+})
+    })();
